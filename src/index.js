ed6af0273016954c3ce24fd2a3fdf6a4a237fee1
Pyro <pyrolovesbuns@gmail.com>
2019-06-07 22:11:15 +0300

    2019

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import injectTapEventPlugin from 'react-tap-event-plugin'
-import { BrowserRouter } from 'react-router-dom'
-import { injectGlobal } from 'styled-components'
-import App from './pages/App'
-import 'normalize.css/normalize.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import injectTapEventPlugin from "react-tap-event-plugin";
+import { BrowserRouter } from "react-router-dom";
+import { injectGlobal } from "styled-components";
+import App from "./pages/App";
+import "normalize.css/normalize.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-injectTapEventPlugin()
+injectTapEventPlugin();
 
-const appElement = document.createElement('div')
-appElement.setAttribute('id', 'app')
-document.body.appendChild(appElement)
+const appElement = document.createElement("div");
+appElement.setAttribute("id", "app");
+document.body.appendChild(appElement);
 
 ReactDOM.render(
   <BrowserRouter>
-    <App/>
+    <App />
   </BrowserRouter>,
-  document.getElementById('app')
-)
+  document.getElementById("app")
+);
 injectGlobal`
   body {
-    background-image: url('https://i.imgur.com/CcUS2ij.png') !important;
+    background-image: url('https://thumbs.gfycat.com/HotAssuredColt-max-1mb.gif') !important;
   }
-`
+`;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,136 +1,156 @@
-import React, { Component } from 'react'
-import styled, { keyframes } from 'styled-components'
-import {
-  Container, Row, Col, Media, Badge
-} from 'reactstrap'
+import React, { Component } from "react";
+import styled, { keyframes } from "styled-components";
+import { Container, Row, Col, Media, Badge } from "reactstrap";
 
 const streamList = [
   {
-    name: 'EA',
-    twitch: 'https://www.twitch.tv/ea',
-    youtube: 'https://www.youtube.com/watch?v=qw16v3oGXso',
-    time: '9 Июня 21:00',
-    image: 'https://i.imgur.com/X62S2PT.png'
+    name: "EA",
+    twitch: "https://www.twitch.tv/ea",
+    youtube: "https://www.youtube.com/watch?v=qw16v3oGXso",
+    time: "8 Июня 19:30",
+    image: "https://i.imgur.com/X62S2PT.png"
   },
   {
-    name: 'Microsoft',
-    twitch: 'https://www.twitch.tv/xbox',
-    youtube: 'https://www.youtube.com/user/xbox/videos',
-    time: '10 Июня 23:00',
-    image: 'https://i.imgur.com/Udn002b.png'
+    name: "Microsoft",
+    twitch: "https://www.twitch.tv/xbox",
+    youtube: "https://www.youtube.com/user/xbox/videos",
+    time: "9 Июня 23:00",
+    image: "https://i.imgur.com/Udn002b.png"
   },
   {
-    name: 'Bethesda',
-    twitch: 'https://www.twitch.tv/bethesda',
-    youtube: 'https://www.youtube.com/bethesda',
-    time: '11 Июня 04:30',
-    image: 'https://i.imgur.com/6BoiPER.png'
+    name: "Bethesda",
+    twitch: "https://www.twitch.tv/bethesda",
+    youtube: "https://www.youtube.com/bethesda",
+    time: "10 Июня 03:30",
+    image: "https://i.imgur.com/6BoiPER.png"
   },
   {
-    name: 'Devolver Digital',
-    twitch: 'https://www.twitch.tv/devolverdigital',
-    youtube: 'https://www.youtube.com/user/DevolverDigital',
-    time: '11 Июня 06:00',
-    image: 'https://i.imgur.com/Ae10JBp.png'
+    name: "Devolver Digital",
+    twitch: "https://www.twitch.tv/devolverdigital",
+    youtube: "https://www.youtube.com/user/DevolverDigital",
+    time: "10 Июня 05:00",
+    image: "https://i.imgur.com/Ae10JBp.png"
   },
   {
-    name: 'Square Enix',
-    twitch: 'https://www.twitch.tv/squareenix',
-    youtube: 'https://www.youtube.com/squareenixpresents',
-    time: '11 Июня 20:00',
-    image: 'https://i.imgur.com/nqROuDQ.jpg'
+    name: "PC Gaming Show",
+    twitch: "https://www.twitch.tv/pcgamer",
+    youtube: "https://www.youtube.com/pcgamer",
+    time: "10 Июня 22:00",
+    image: "https://i.imgur.com/lGYxmFb.png"
   },
   {
-    name: 'Ubisoft',
-    twitch: 'https://www.twitch.tv/ubisoft',
-    youtube: 'https://www.youtube.com/channel/UCBMvc6jvuTxH6TNo9ThpYjg',
-    time: '11 Июня 23:00',
-    image: 'https://i.imgur.com/2YeWBmJ.png'
+    name: "Square Enix",
+    twitch: "https://www.twitch.tv/squareenix",
+    youtube: "https://www.youtube.com/squareenixpresents",
+    time: "11 Июня 04:00",
+    image: "https://i.imgur.com/nqROuDQ.jpg"
   },
   {
-    name: 'PC Gaming Show',
-    twitch: 'https://www.twitch.tv/pcgamer',
-    youtube: 'https://www.youtube.com/pcgamer',
-    time: '12 Июня 01:00',
-    image: 'https://i.imgur.com/lGYxmFb.png'
+    name: "Nintendo",
+    twitch: "https://www.twitch.tv/nintendo",
+    youtube: "https://www.twitch.tv/nintendo",
+    time: "11 Июня 19:00",
+    image: "https://i.imgur.com/RmAYpZh.png"
   },
   {
-    name: 'Sony',
-    twitch: 'https://www.twitch.tv/playstation',
-    youtube: 'https://www.youtube.com/user/PlayStation',
-    time: '12 Июня 04:00',
-    image: 'https://i.imgur.com/QBmNYes.png'
+    name: "Ubisoft",
+    twitch: "https://www.twitch.tv/ubisoft",
+    youtube: "https://www.youtube.com/channel/UCBMvc6jvuTxH6TNo9ThpYjg",
+    time: "11 Июня 23:00",
+    image: "https://i.imgur.com/2YeWBmJ.png"
   },
   {
-    name: 'Nintendo',
-    twitch: 'https://www.twitch.tv/nintendo',
-    youtube: 'https://www.twitch.tv/nintendo',
-    time: '12 Июня 17:00',
-    image: 'https://i.imgur.com/RmAYpZh.png'
-  },
-]
+    name: "Сони пидорасы",
+    image: "https://up.vidyagam.es/images/logo_smol.png",
+    pizda: true
+  }
+];
 
 class Home extends Component {
-  constructor () {
-    super()
+  constructor() {
+    super();
   }
 
-  render () {
+  render() {
     return (
       <Container>
-        <div style={{textAlign: 'center'}}>
-          <img width='200' height='200' src={'https://i.imgur.com/ruTRMib.png'}/>
-          <h5><Badge color={'danger'}>E3 fucking sucks</Badge></h5>
-          <iframe width="0" height="0"
-                  src="https://www.youtube.com/embed/odGExIQTxvg?rel=0&amp;controls=0&amp;showinfo=0&autoplay=1&volume=50"
-                  frameBorder="0"
-                  allow="autoplay; encrypted-media" allowFullScreen></iframe>
+        <div style={{ textAlign: "center" }}>
+          <img
+            width="200"
+            height="200"
+            src={"https://i.imgur.com/ruTRMib.png"}
+          />
+          <h5>
+            <Badge color={"danger"}>E3 still fucking sucks</Badge>
+          </h5>
+          <audio autoPlay={true}>
+            <source
+              src="https://drop3.dropmefiles.com/upl/B/B4COc80wgiHs/BmbnH_o_1dcplae6215091bd411aj1t4214lma"
+              type="audio/mpeg"
+            />
+          </audio>
         </div>
         {streamList.map(item => (
           <Wrap>
             <Media>
               <Media left href="#">
-                <Media object src={item.image} alt="Generic placeholder image"/>
+                <Media
+                  object
+                  src={item.image}
+                  alt="Generic placeholder image"
+                />
               </Media>
-              <Media className={'show-body'} body>
-                <Media heading className={'show-name'}>
+              <Media className={"show-body"} body>
+                <Media heading className={"show-name"}>
                   {item.name}
                 </Media>
                 <GridRow>
                   <FieldName>Time</FieldName>
                   <div>{item.time}</div>
                 </GridRow>
-                <GridRow>
-                  <FieldName>Twitch</FieldName>
-                  <a href={item.twitch} target={'_blank'}>Link</a>
-                </GridRow>
-                <GridRow>
-                  <FieldName>Youtube</FieldName>
-                  <a href={item.youtube} target={'_blank'}>Link</a>
-                </GridRow>
+                {item.twitch && (
+                  <GridRow>
+                    <FieldName>Twitch</FieldName>
+                    <a href={item.twitch} target={"_blank"}>
+                      Link
+                    </a>
+                  </GridRow>
+                )}
+                {item.youtube && (
+                  <GridRow>
+                    <FieldName>Youtube</FieldName>
+                    <a href={item.youtube} target={"_blank"}>
+                      Link
+                    </a>
+                  </GridRow>
+                )}
+                {item.pizda && (
+                  <GridRow>
+                    <h1>ПОШЕЛ НАХУЙ</h1>
+                  </GridRow>
+                )}
               </Media>
             </Media>
           </Wrap>
         ))}
-
       </Container>
-    )
+    );
   }
 }
 
 const GridRow = styled.div`
   display: grid;
-  grid: 1fr / 150px 2fr ;
+  grid: 1fr / 150px 2fr;
   margin: 15px 0px;
   @media only screen and (max-width: 700px) {
-     display: flex;
-     flex-direction: column;
+    display: flex;
+    flex-direction: column;
   }
-`
+`;
 
 const FieldName = styled.div`
   font-weight: bold;
-`
+`;
 
 const Wrap = styled.div`
   margin: 20px 0px;
@@ -141,12 +161,12 @@ const Wrap = styled.div`
     padding-top: 20px;
   }
   & .show-body {
-    padding-left: 20px
+    padding-left: 20px;
   }
   & img {
     width: 100px;
     height: 100px;
   }
-`
+`;
 
-export default Home
+export default Home;
